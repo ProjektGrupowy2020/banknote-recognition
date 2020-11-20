b78789545893117a847ecf685bab4c82932da6eb
Jacek <jacekard@gmail.com>
2020-11-20 19:25:47 +0100

    sounds & vibration

--- android/app/src/main/java/com/example/flutterapp/MainActivity.java
+++ android/app/src/main/java/com/example/flutterapp/MainActivity.java
@@ -23,6 +23,7 @@ import java.util.HashMap;
 import android.os.Bundle;
 
 public class MainActivity extends FlutterActivity {
+    private static final boolean isDebug = false; 
     private static final String CHANNEL = "samples.flutter.dev/battery";
     private static final LinkedBlockingQueue<Runnable> neuralNetFrameQueue = new LinkedBlockingQueue<Runnable>(1);
     Module module;
@@ -124,13 +125,17 @@ public class MainActivity extends FlutterActivity {
 
         long stop = System.currentTimeMillis();
         long time = stop - start;
-        Log.d("Time", "Convert bitmap to float tensor: " +time);
+        if(isDebug) {
+            Log.d("Time", "Convert bitmap to float tensor: " +time);
+        }
 
         Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
         long sxd = System.currentTimeMillis();
         time = sxd - stop;
 
-        Log.d("Time", "Inference: " + time);
+        if(isDebug) {
+            Log.d("Time", "Inference: " + time);
+        }
 
         float[] scores = outputTensor.getDataAsFloatArray();
 
@@ -141,12 +146,13 @@ public class MainActivity extends FlutterActivity {
                 max = scores[i];
                 maxInd = i;
             }
-            //result[i] = scores[i];
-            Log.d("SCORES", String.format("Class: %d -----  %f", i, scores[i]));
+            if(isDebug) {
+                Log.d("SCORES", String.format("Class: %d -----  %f", i, scores[i]));
+            }
         }
-        //int maxIdx = scores.indices.maxBy { scores[it] } ?: -1; /// co to ??
-        Log.d("Prediction", String.format("Class: %d -----  %f", maxInd, scores[maxInd]));
-
+        if(isDebug) {
+            Log.d("Prediction", String.format("Class: %d -----  %f", maxInd, scores[maxInd]));
+        }
         return maxInd;
     }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'dart:async';
 import 'dart:io';
-import 'dart:typed_data';
-import 'package:flutterapp/imagenet.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:path/path.dart' show join;
-import 'package:path_provider/path_provider.dart';
+import 'package:vibration/vibration.dart';
+import 'package:soundpool/soundpool.dart';
 
 Future<void> main() async {
   // Ensure that plugin services are initialized so that `availableCameras()`
@@ -45,6 +43,7 @@ class TakePictureScreen extends StatefulWidget {
 
 class TakePictureScreenState extends State<TakePictureScreen> {
   CameraController _controller;
+  Soundpool soundpool;
   int frames = 0;
   Future<void> _initializeControllerFuture;
   static const platform = const MethodChannel('samples.flutter.dev/battery');
@@ -53,25 +52,106 @@ class TakePictureScreenState extends State<TakePictureScreen> {
   int _iter = 0;
   String error;
   bool busy = false;
+  // displaying values
+  List<String> money = ["10", "20", "50", "100", "200", "500", "None"];
+  List<int> _currentPredictionsList = new List<int>();
+  int maxCorrectPredictionsInSequence =
+      4; // this is arbitrary and can be changed.
+  String defaultDisplayMessage = "Skieruj kamerę na banknot!";
+  String currentDisplayMessage = "";
+  bool hasDetectedPrediction = false;
+  // vibration
+  bool hasVibration = false;
+  bool hasCustomVibrationsSupport = false;
+  Map vibrationmap = new Map();
+  // sound
+  Map soundmap = new Map();
+
+  void predictionActions(String value) async {
+    // play sound depending on value.
+    await this.soundpool.play(this.soundmap[value]);
+    // vibrate depending on value.
+    if (this.hasVibration && this.hasCustomVibrationsSupport) {
+      Vibration.vibrate(pattern: this.vibrationmap[value]);
+    }
+  }
+
+  void updatePredictionMessage(int prediction) async {
+    // if has detected prediction previously, do nothing.
+    if (this.hasDetectedPrediction) {
+      return;
+    }
+
+    if (this.money[_prediction] == 'None') {
+      this.currentDisplayMessage = defaultDisplayMessage;
+      await this.soundpool.play(this.soundmap['None']);
+    } else {
+      predictionActions(this.money[_prediction]);
+      setState(() {
+        this.currentDisplayMessage =
+            "Banknot " + money[_prediction] + " złotowy";
+      });
+    }
+  }
 
   Future<void> _getPrediction(CameraImage cameraImage) async {
-//    print("kurwaaa");
     var framesY = cameraImage.planes[0].bytes;
     var framesU = cameraImage.planes[1].bytes;
     var framesV = cameraImage.planes[2].bytes;
     try {
-      platform.invokeMethod('getPrediction',
-          <String, dynamic>{'width':cameraImage.width, 'height':cameraImage.height,
-                            'Y': framesY,'U': framesU,'V': framesV});
-      //await platform.invokeMethod('getPrediction', {'file': file});
+      platform.invokeMethod('getPrediction', <String, dynamic>{
+        'width': cameraImage.width,
+        'height': cameraImage.height,
+        'Y': framesY,
+        'U': framesU,
+        'V': framesV
+      });
     } on PlatformException catch (e) {
-      print("kurwaaa213 error: ${e.message}");
+      print("error: ${e.message}");
       error = e.message;
     }
 
     setState(() {
       busy = true;
     });
+  }
+
+  void checkDeviceVibrationOptions() async {
+    if (await Vibration.hasVibrator()) {
+      this.hasVibration = true;
+      if (await Vibration.hasCustomVibrationsSupport()) {
+        this.hasCustomVibrationsSupport = true;
+      }
+    }
+  }
+
+  void loadSoundAssets() async {
+    this.soundpool = Soundpool(streamType: StreamType.notification);
+    ByteData asset;
+    asset = await rootBundle.load("sounds/10.wav");
+    this.soundmap["10"] = await soundpool.load(asset);
+    asset = await rootBundle.load("sounds/20.wav");
+    this.soundmap["20"] = await soundpool.load(asset);
+    asset = await rootBundle.load("sounds/50.wav");
+    this.soundmap["50"] = await soundpool.load(asset);
+    asset = await rootBundle.load("sounds/100.wav");
+    this.soundmap["100"] = await soundpool.load(asset);
+    asset = await rootBundle.load("sounds/200.wav");
+    this.soundmap["200"] = await soundpool.load(asset);
+    asset = await rootBundle.load("sounds/500.wav");
+    this.soundmap["500"] = await soundpool.load(asset);
+    asset = await rootBundle.load("sounds/None.wav");
+    this.soundmap["None"] = await soundpool.load(asset);
+  }
+
+  void loadVibrationOptions() {
+    // in millis.
+    this.vibrationmap["10"] = [0, 300];
+    this.vibrationmap["20"] = [0, 300, 300, 300];
+    this.vibrationmap["50"] = [0, 300, 300, 300, 300, 300];
+    this.vibrationmap["100"] = [0, 1000, 500, 1000];
+    this.vibrationmap["200"] = [0, 1000, 500, 1000, 500, 1000];
+    this.vibrationmap["500"] = [0, 3000];
   }
 
   @override
@@ -88,11 +168,39 @@ class TakePictureScreenState extends State<TakePictureScreen> {
     // Next, initialize the controller. This returns a Future.
     _initializeControllerFuture = _controller.initialize();
 
-    platform.setMethodCallHandler((MethodCall call) async{
-      if(call.method == "predictionResult"){
+    this.checkDeviceVibrationOptions();
+    this.currentDisplayMessage = defaultDisplayMessage;
+    this.loadSoundAssets();
+    this.loadVibrationOptions();
+
+    platform.setMethodCallHandler((MethodCall call) async {
+      if (call.method == "predictionResult") {
         final args = call.arguments;
+        int currentPrediction = args["result"];
+        if (this._currentPredictionsList.isEmpty) {
+          debugPrint('predictions list empty');
+          this._currentPredictionsList.add(currentPrediction);
+        } else if (this._currentPredictionsList.contains(currentPrediction)) {
+          debugPrint('predictions list matching previous');
+          this._currentPredictionsList.add(currentPrediction);
+        } else {
+          debugPrint('Clear predictions list');
+          String lastValue = money[currentPrediction];
+          debugPrint('Last value: $lastValue');
+          this._currentPredictionsList.clear();
+          debugPrint('Size of list: +' +
+              this._currentPredictionsList.length.toString());
+          this.hasDetectedPrediction = false;
+        }
+
+        if (this._currentPredictionsList.length >=
+            this.maxCorrectPredictionsInSequence) {
+          this._prediction = this._currentPredictionsList.last;
+          this.updatePredictionMessage(this._prediction);
+          this.hasDetectedPrediction = true;
+        }
+
         setState(() {
-          _prediction = args["result"];
           _iter = _iter + 1;
           busy = false;
         });
@@ -100,18 +208,13 @@ class TakePictureScreenState extends State<TakePictureScreen> {
       return true;
     });
 
-    _initializeControllerFuture.then( (x) {
-
+    _initializeControllerFuture.then((x) {
       _controller.startImageStream((CameraImage availableImage) async {
-        if(busy)
-            return;
+        if (busy) return;
         frames = 0;
         await _getPrediction(availableImage);
       });
-
-    }
-    );
-
+    });
   }
 
   @override
@@ -124,9 +227,8 @@ class TakePictureScreenState extends State<TakePictureScreen> {
 
   @override
   Widget build(BuildContext context) {
-    List<String> money =["10", "20", "50", "100", "200", "500", "None"];
     return Scaffold(
-      appBar: AppBar(title: Text('${money[_prediction]} $_iter')),
+      appBar: AppBar(title: Text(this.currentDisplayMessage)),
       // Wait until the controller is initialized before displaying the
       // camera preview. Use a FutureBuilder to display a loading spinner
       // until the controller has finished initializing.
